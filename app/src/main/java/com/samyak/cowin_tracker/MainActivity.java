4a118ad18a839aa48519db83174223676e7986ed
Samyak Jain <jainsamyak2300@gmail.com>
2021-05-06 23:14:28 +0530

    Database Update and Pop Up Window Complete

--- app/src/main/java/com/samyak/cowin_tracker/MainActivity.java
+++ app/src/main/java/com/samyak/cowin_tracker/MainActivity.java
@@ -2,7 +2,6 @@ package com.samyak.cowin_tracker;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -10,13 +9,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.PopupWindow;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
@@ -34,19 +37,20 @@ public class MainActivity extends AppCompatActivity {
     public static final String ANONYMOUS = "anonymous";
     public static final int RC_SIGN_IN = 1;
 
-    private static final String TAG = "PhoneAuthActivity";
+    private static final String TAG = "MainActivity";
     private String mUsername;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mUsersDatabaseReference;
+    private DatabaseReference mUsersDatabaseReference, mTrackPincodesDatabaseReference;
 
-    private FloatingActionButton addPincodeButton;
+    private FloatingActionButton addPincodeFloatingButton;
     private PopupWindow popupWindow;
     private Context context;
     private RelativeLayout mainLayout;
+    private Button addPincodeButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,8 +65,9 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseAuth.getInstance();
 
         mUsersDatabaseReference = mFirebaseDatabase.getReference().child("users");
+        mTrackPincodesDatabaseReference = mFirebaseDatabase.getReference().child("Track Pin Codes");
 
-        addPincodeButton = findViewById(R.id.addPincode);
+        addPincodeFloatingButton = findViewById(R.id.addPincode);
 
         mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
 
@@ -89,24 +94,73 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        addPincodeButton.setOnClickListener(new View.OnClickListener() {
+        addPincodeFloatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
 
                 View popUpView = inflater.inflate(R.layout.pincode_pop_up, null);
 
-                popupWindow = new PopupWindow(
-                        popUpView,
-                        ActionBar.LayoutParams.WRAP_CONTENT,
-                        ActionBar.LayoutParams.WRAP_CONTENT
-                );
 
-                if (Build.VERSION.SDK_INT >= 21) {
-                    popupWindow.setElevation(5.0f);
-                }
+                popupWindow = new PopupWindow(popUpView);
+                popupWindow.setWidth(RelativeLayout.LayoutParams.WRAP_CONTENT);
+                popupWindow.setHeight(RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                popupWindow.setElevation(5.0f);
+
+
+                addPincodeButton = popUpView.findViewById(R.id.addPincodeButton);
+                ImageButton closePopUpImageButton = popUpView.findViewById(R.id.closePopupButton);
+
+                EditText pincodeInput = popUpView.findViewById(R.id.pincodeInput);
+                RadioGroup radioGroup = popUpView.findViewById(R.id.radioGroup);
+
+                addPincodeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String pincode = pincodeInput.getText().toString();
+                        int selected_radio_button_id = radioGroup.getCheckedRadioButtonId();
+
+
+                        if (pincode.equals("")) {
+                            Toast.makeText(MainActivity.this, "Enter a valid Pin code", Toast.LENGTH_SHORT).show();
+                        } else if (selected_radio_button_id == -1) {
+                            Toast.makeText(MainActivity.this, "Please Select a Slot to Track", Toast.LENGTH_SHORT).show();
+                        } else {
+                            String userId = Objects.requireNonNull(mFirebaseAuth.getCurrentUser()).getUid();
+                            RadioButton radioButton = (RadioButton) popUpView.findViewById(selected_radio_button_id);
+                            String radioText = radioButton.getText().toString();
+
+                            switch (radioText) {
+                                case "18+ Slots":
+                                    mTrackPincodesDatabaseReference.child(pincode).child("is_18_plus").child(userId).setValue(userId);
+                                    mUsersDatabaseReference.child(userId).child(pincode).setValue("is_18_plus");
+                                    break;
+                                case "45+ Slots":
+                                    mTrackPincodesDatabaseReference.child(pincode).child("is_45_plus").child(userId).setValue(userId);
+                                    mUsersDatabaseReference.child(userId).child(pincode).setValue("is_45_plus");
+                                    break;
+                                case "Both":
+                                    mTrackPincodesDatabaseReference.child(pincode).child("is_all").child(userId).setValue(userId);
+                                    mUsersDatabaseReference.child(userId).child(pincode).setValue("is_all");
+                                    break;
+                            }
+                            popupWindow.dismiss();
+                        }
+                    }
+                });
+
+                closePopUpImageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
 
                 popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+                popupWindow.setFocusable(true);
+                popupWindow.update();
 
             }
         });
@@ -122,14 +176,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        switch (item.getItemId()) {
-            case R.id.sign_out_menu:
-                AuthUI.getInstance().signOut(this);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.sign_out_menu) {
+            AuthUI.getInstance().signOut(this);
+            return true;
         }
+        return super.onOptionsItemSelected(item);
 //
     }
 
